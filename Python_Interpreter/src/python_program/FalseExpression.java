fb92e73d483a7e3fde4e75f6008227f7b1fea346
DominikaMol <DominikaMol@1884e9d7-1fcd-b472-f8f2-a483660a54fd>
2012-04-14 22:12:59 +0000

    Struktury do wyrażeń logicznych

---
+++ Python_Interpreter/src/python_program/FalseExpression.java
@@ -0,0 +1,14 @@
+package python_program;
+
+public class FalseExpression extends Expression {
+	
+	public FalseExpression(){
+		
+	}
+	
+	@Override
+	public void print() {
+		System.out.print("False");
+	}
+
+}
---
+++ Python_Interpreter/src/python_program/TrueExpression.java
@@ -0,0 +1,14 @@
+package python_program;
+
+public class TrueExpression extends Expression {
+	
+	public TrueExpression(){
+		
+	}
+	
+	@Override
+	public void print() {
+		System.out.print("True");
+	}
+
+}
