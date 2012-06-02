213ec457596e986886f1b068ee50f334af7e86b1
DominikaMol <DominikaMol@1884e9d7-1fcd-b472-f8f2-a483660a54fd>
2012-06-02 16:05:48 +0000

    instrukcja przypisania

---
+++ trunk/Python_Interpreter/src/python_program/statement/AssignStatement.java
@@ -0,0 +1,23 @@
+package python_program.statement;
+
+import python_program.expression.Expression;
+import python_program.expression.IdentifierExpression;
+
+public class AssignStatement extends SimpleStatement {
+	
+	IdentifierExpression identifier;
+	Expression expression;
+	
+	public AssignStatement(IdentifierExpression identifier, Expression expression){
+		this.identifier = identifier;
+		this.expression = expression;
+	}
+	
+	@Override
+	public void print() {
+		this.identifier.print();
+		System.out.print(" = ");
+		this.expression.print();
+	}
+
+}
