cc584f4e51a80e29286cf1e6cfaedb3cfab77efa
DominikaMol <DominikaMol@1884e9d7-1fcd-b472-f8f2-a483660a54fd>
2012-09-15 20:14:11 +0000

    Target assign statement

---
+++ trunk/Python_Interpreter/src/python_program/statement/TargetAssignStatement.java
@@ -0,0 +1,22 @@
+package python_program.statement;
+
+import python_program.expression.Expression;
+
+public class TargetAssignStatement extends Statement {
+	
+	Expression expr1;
+	Expression expr2;
+	
+	public TargetAssignStatement(Expression expr1, Expression expr2) {
+		this.expr1= expr1;
+		this.expr2 = expr2;
+	}
+
+	@Override
+	public void print() {
+		this.expr1.print();
+		System.out.print(" = ");
+		this.expr2.print();
+	}
+
+}
