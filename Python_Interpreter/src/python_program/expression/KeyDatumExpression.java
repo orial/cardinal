5fc4e1cf2e3a5b395077fc666bc7800feb22e0b1
DominikaMol <DominikaMol@1884e9d7-1fcd-b472-f8f2-a483660a54fd>
2012-09-15 22:23:09 +0000

    parsowanie pustej listy (i 240 konfliktów ;P)

---
+++ Python_Interpreter/src/python_program/expression/KeyDatumExpression.java
@@ -0,0 +1,27 @@
+package python_program.expression;
+
+import python_program.Types;
+
+public class KeyDatumExpression extends Expression {
+	
+	Expression expr1;
+	Expression expr2;
+	
+	public KeyDatumExpression(Expression expr1, Expression expr2) {
+		this.expr1 = expr1;
+		this.expr2 = expr2;
+	}
+
+	@Override
+	public void print() {
+		expr1.print();
+		System.out.print(":");
+		expr2.print();
+	}
+
+	@Override
+	public Types getType() {
+		return Types.none_type;
+	}
+
+}
