f00dddf5572bd8f248b74ba3e6803d52b76fe00a
DominikaMol <DominikaMol@1884e9d7-1fcd-b472-f8f2-a483660a54fd>
2012-09-16 17:09:26 +0000

    definicje funkcji

---
+++ trunk/Python_Interpreter/src/python_program/statement/FunctionDefinitionStatement.java
@@ -0,0 +1,42 @@
+package python_program.statement;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class FunctionDefinitionStatement extends Statement {
+	
+	String identifier;
+	List<String> identifierList;
+	List<Statement> statementList;
+	
+	public FunctionDefinitionStatement(String identifier, List<String> identifierList, List<Statement> statementList) {
+		this.identifier = identifier;
+		this.identifierList = identifierList;
+		this.statementList = statementList;
+	}
+	
+	@Override
+	public void print() {
+		System.out.print("def ");
+		System.out.print(this.identifier);
+		System.out.print(" (");
+		Iterator<String> identifierIterator = identifierList.iterator();
+		if(identifierIterator.hasNext()) {
+			System.out.print(identifierIterator.next());
+		}
+		while(identifierIterator.hasNext()) {
+			System.out.print(", ");
+			System.out.print(identifierIterator.next());
+		}
+		System.out.println(":");
+		
+		System.out.println("<<");
+		Iterator<Statement> statementIterator = this.statementList.iterator();
+		while(statementIterator.hasNext()){
+			statementIterator.next().print();
+			System.out.println();
+		}
+		System.out.print(">>");
+	}
+
+}
